8ca605e729f67413eef70df84b482231fc96ae6a
LornahMunene <munenelornah9@gmail.com>
2024-04-03 09:35:15 +0300

    feat:array-lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+let cats=["Milo","Otis","Garfield"]
+function destructivelyAppendCat(){
+  cats.push("Ralph")
+}
+function destructivelyPrependCat(){
+  cats.unshift("Bob")
+}
+function destructivelyRemoveLastCat(){
+  cats.pop()
+}
+function destructivelyRemoveFirstCat(){
+  cats.shift()
+}
+function appendCat(){
+  let copyOfCats=[...cats]
+copyOfCats.push("Broom")
+return copyOfCats
+}
+function prependCat(){
+  let copyOfCats=[...cats]
+  copyOfCats.unshift("Arnold")
+  return copyOfCats
+}
+function removeLastCat(){
+  let copyOfCats=[...cats]
+  copyOfCats.pop()
+  return copyOfCats
+}
+function removeFirstCat(){
+  let copyOfCats=[...cats]
+  copyOfCats.shift()
+  return copyOfCats
+}
